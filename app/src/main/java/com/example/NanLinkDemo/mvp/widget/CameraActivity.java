8100c148f3ebe442e244f2115ad8495b6721307a
GitHub-JYB <977323939@qq.com>
2023-08-15 20:13:03 +0800

    update

--- app/src/main/java/com/example/NanLinkDemo/mvp/widget/CameraActivity.java
+++ app/src/main/java/com/example/NanLinkDemo/mvp/widget/CameraActivity.java
@@ -1,6 +1,7 @@
 package com.example.NanLinkDemo.mvp.widget;
 
 import android.content.Context;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
@@ -9,9 +10,12 @@ import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
+import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,6 +34,7 @@ import com.example.NanLinkDemo.mvp.view.CameraView;
 import com.example.NanLinkDemo.util.Constant;
 
 import java.io.IOException;
+import java.util.List;
 
 @Route(path = Constant.ACTIVITY_URL_Camera)
 public class CameraActivity extends BaseActivity<ActivityCameraBinding> implements CameraView, View.OnClickListener {
@@ -41,6 +46,21 @@ public class CameraActivity extends BaseActivity<ActivityCameraBinding> implemen
     int id;
 
     private CameraPresenterImpl presenter;
+    private Camera.CameraInfo cameraInfo;
+    private Camera mCamera;
+    private SurfaceHolder holder;
+
+    Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+        @Override
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            if (data != null){
+                Camera.Parameters parameters = camera.getParameters();
+                Camera.Size size = parameters.getPreviewSize();
+            }
+            camera.addCallbackBuffer(data);
+        }
+    };
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -78,16 +98,12 @@ public class CameraActivity extends BaseActivity<ActivityCameraBinding> implemen
         ViewGroup.LayoutParams layoutParams = binding.preview.getLayoutParams();
         layoutParams.height = layoutParams.width = MyApplication.widthPixels;
         binding.preview.setLayoutParams(layoutParams);
-        SurfaceHolder holder = binding.preview.getHolder();
+        holder = binding.preview.getHolder();
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         holder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
-                try {
-                    
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                openCamera(Camera.CameraInfo.CAMERA_FACING_BACK);
             }
 
             @Override
@@ -97,9 +113,131 @@ public class CameraActivity extends BaseActivity<ActivityCameraBinding> implemen
 
             @Override
             public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
-
+                releaseCamera();
             }
         });
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null){
+            mCamera.stopPreview();
+            mCamera.stopFaceDetection();
+            mCamera.setPreviewCallback(null);
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    private void openCamera(int cameraId) {
+        try {
+            releaseCamera();
+            if (cameraInfo == null){
+                cameraInfo = new Camera.CameraInfo();
+            }
+            int numberOfCameras = Camera.getNumberOfCameras();
+            for (int i = 0; i < numberOfCameras; i++){
+                Camera.getCameraInfo(i, cameraInfo);
+                if (cameraInfo.facing == cameraId){
+                    mCamera = Camera.open(i);
+                    break;
+                }
+            }
+
+            startPreview();
+        }catch (Exception e){
+            e.printStackTrace();
+            mCamera = null;
+        }
+    }
+
+    private void startPreview() {
+        try {
+            WindowManager manager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+            Display display = manager.getDefaultDisplay();
+            int width = display.getWidth();
+            int height = display.getHeight();
+
+            initPreviewParams(width, height);
+
+            adjustCameraOrientation();
+
+            mCamera.setPreviewDisplay(holder);
+
+            mCamera.addCallbackBuffer(new byte[width * height * 3 / 2]);
+
+            mCamera.setPreviewCallbackWithBuffer(previewCallback);
+
+            mCamera.startPreview();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void adjustCameraOrientation() {
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+
+        int degress = 0;
+
+        switch (rotation){
+            case Surface.ROTATION_0:
+                degress = 0;
+                break;
+            case Surface.ROTATION_90:
+                degress = 90;
+                break;
+            case Surface.ROTATION_180:
+                degress = 180;
+                break;
+            case Surface.ROTATION_270:
+                degress = 270;
+                break;
+        }
+
+        int result = 0;
+
+        if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK){
+            result = (cameraInfo.orientation - degress + 360 ) % 360;
+        }else if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT){
+            result = (cameraInfo.orientation + degress) % 360;
+            result = (360 - result) % 360;
+        }
+
+        mCamera.setDisplayOrientation(result);
+    }
+
+    private void initPreviewParams(int width, int height) {
+        if (mCamera != null){
+            Camera.Parameters parameters = mCamera.getParameters();
+            List<Camera.Size> sizes = parameters.getSupportedPreviewSizes();
+            Camera.Size bestSize = getBestSize(width, height, sizes);
+            parameters.setPreviewSize(bestSize.width, bestSize.height);
+            parameters.setPictureSize(bestSize.width, bestSize.height);
+            parameters.setPreviewFormat(ImageFormat.NV21);
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+            mCamera.setParameters(parameters);
+        }
+    }
+
+    private Camera.Size getBestSize(int width, int height, List<Camera.Size> sizes) {
+        Camera.Size bestSize = null;
+        float uiRadio = (float) height / width;
+        float minRadio = uiRadio;
+        for (Camera.Size previewSize : sizes){
+            float cameraRadio = (float) previewSize.width / previewSize.height;
+
+            float offset = Math.abs(cameraRadio - minRadio);
+            if (offset < minRadio){
+                minRadio = offset;
+                bestSize = previewSize;
+            }
+
+            if (uiRadio == cameraRadio){
+                bestSize = previewSize;
+                break;
+            }
+        }
+        return bestSize;
+
     }
 
 
